0544d15055541372033e994ec03a65f390931ca6
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2014-07-19 17:30:57 +0700

    entering main activity

---
+++ src/com/example/game/GameData.java
@@ -0,0 +1,70 @@
+package com.example.game;
+
+public interface GameData {
+	
+	Question[] QUESTIONS = {
+			new Question("Apakah nama Ibukota\nProvinsi Bali?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"
+					}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Banten?", 
+					new String[]{
+					"Jakarta", 
+					"Bandung", 
+					"Bogor", 
+					"Banten"
+					}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Jawa Timur?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Kalimantan Timur?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Bangka Belitung?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Maluku Utara?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi\nNusa Tenggara Barat?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Papua?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Sulawesi Barat?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+			new Question("Apakah nama Ibukota\nProvinsi Sumatera Selatan?", 
+					new String[]{
+					"Magelang", 
+					"Solo", 
+					"Yogya", 
+					"Denpasar"}, 3),
+	};
+
+}
--- src/com/example/game/MainActivity.java
+++ src/com/example/game/MainActivity.java
@@ -1,14 +1,5 @@
 package com.example.game;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.andengine.engine.camera.Camera;
-import org.andengine.engine.options.EngineOptions;
-import org.andengine.engine.options.ScreenOrientation;
-import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
-import org.andengine.entity.scene.Scene;
-import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.entity.text.TextOptions;
@@ -18,179 +9,71 @@ import org.andengine.opengl.font.FontFactory;
 import org.andengine.opengl.texture.ITexture;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-import org.andengine.opengl.texture.bitmap.BitmapTexture;
 import org.andengine.opengl.texture.region.ITextureRegion;
-import org.andengine.opengl.texture.region.TextureRegionFactory;
-import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.HorizontalAlign;
-import org.andengine.util.adt.io.in.IInputStreamOpener;
-import org.andengine.util.debug.Debug;
-
 import android.content.Intent;
 import android.graphics.Color;
 
-public class MainActivity extends SimpleBaseGameActivity implements Constants{
+public class MainActivity extends GameActivityModel implements GameData{
 
-	private Font mTitleFont;
 	private Font mSubFont;
 	private Font mQuestionFont;
 	
 	final static int OPTIONS_COUNT = 4;
 	
-	private ITexture mQuestionFrameTexture;
-	private ITexture[] mOptionsFrameTexture = new ITexture[OPTIONS_COUNT];
 	private ITextureRegion mQuestionFrameTextureRegion;
 	private ITextureRegion[] mOptionsFrameTextureRegion = new ITextureRegion[OPTIONS_COUNT];
-	
-	Question[] questions = {
-			new Question("Apakah nama Ibukota\nProvinsi Bali?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"
-					}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Banten?", 
-					new String[]{
-					"Jakarta", 
-					"Bandung", 
-					"Bogor", 
-					"Banten"
-					}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Jawa Timur?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Kalimantan Timur?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Bangka Belitung?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Maluku Utara?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Nusa Tenggara Barat?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Papua?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Sulawesi Barat?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-			new Question("Apakah nama Ibukota\nProvinsi Sumatera Selatan?", 
-					new String[]{
-					"Magelang", 
-					"Solo", 
-					"Yogya", 
-					"Denpasar"}, 3),
-	};
 
 	@Override
-	public EngineOptions onCreateEngineOptions() {
-		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
-	}
-
-	@Override
-	protected void onCreateResources() {
-		
-		try {
-			this.mQuestionFrameTexture = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
-				@Override
-				public InputStream open() throws IOException {
-					return getAssets().open("gfx/question_frame.png");
-				}
-			});
-			this.mQuestionFrameTexture.load();
-			this.mQuestionFrameTextureRegion = TextureRegionFactory.extractFromTexture(this.mQuestionFrameTexture);
-		} catch (IOException e) {
-			Debug.e(e);
-		}
+	protected void init_resources() {
+		mQuestionFrameTextureRegion = texture_region("gfx/question_frame.png");
 		
 		for(int i = 0;i < OPTIONS_COUNT;i++){
-			try{
-				this.mOptionsFrameTexture[i] = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
-					@Override
-					public InputStream open() throws IOException {
-						return getAssets().open("gfx/option_frame.png");
-					}
-				});
-				this.mOptionsFrameTexture[i].load();
-				this.mOptionsFrameTextureRegion[i] = TextureRegionFactory.extractFromTexture(this.mOptionsFrameTexture[i]);
-			} catch (IOException e){
-				Debug.e(e);
-			}
+			this.mOptionsFrameTextureRegion[i] = texture_region("gfx/option_frame.png");
 		}
 		
-		final ITexture fontTitleTexture = new BitmapTextureAtlas(this.getTextureManager(), 256, 256, TextureOptions.BILINEAR);
 		final ITexture fontSubTexture = new BitmapTextureAtlas(this.getTextureManager(), 256, 256, TextureOptions.BILINEAR);
 		final ITexture fontQuestionTexture = new BitmapTextureAtlas(this.getTextureManager(), 256, 256, TextureOptions.BILINEAR);
 		FontFactory.setAssetBasePath("font/");
-		this.mTitleFont = FontFactory.createFromAsset(this.getFontManager(), fontTitleTexture, this.getAssets(), "Forque.ttf", 30, true, Color.BLACK);
-		this.mTitleFont.load();
-		this.mSubFont = FontFactory.createFromAsset(this.getFontManager(), fontSubTexture, this.getAssets(), "OpenSans-Regular.ttf", 20, true, Color.BLACK);
+		this.mSubFont = FontFactory.createFromAsset(this.getFontManager(), fontSubTexture, this.getAssets(), "Anonymous.ttf", 18, true, Color.BLACK);
 		this.mSubFont.load();
-		this.mQuestionFont = FontFactory.createFromAsset(this.getFontManager(), fontQuestionTexture, this.getAssets(), "OpenSans-Regular.ttf", 14, true, Color.BLACK);
+		this.mQuestionFont = FontFactory.createFromAsset(this.getFontManager(), fontQuestionTexture, this.getAssets(), "OpenSans-Regular.ttf", 16, true, Color.BLACK);
 		this.mQuestionFont.load();
 	}
 	
 	static int question_number;
 
 	@Override
-	protected Scene onCreateScene() {
+	protected void init_scene(){
+		int level = 1;
 		question_number = 0;
 		
-		final Scene scene = new Scene();
-		scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
-		
 		final Sprite question_frame = new Sprite(0, 0, this.mQuestionFrameTextureRegion, this.getVertexBufferObjectManager());
-		scene.attachChild(question_frame);
-		question_frame.setPosition(CAMERA_WIDTH/2 - question_frame.getWidth()/2, CAMERA_HEIGHT/2 - question_frame.getHeight()/2);
+		attach(question_frame, Alignment.CENTER);set_position(question_frame, 0, -30);
 		
-		final Text title = new Text(0, 0, mTitleFont, "Main", this.getVertexBufferObjectManager());
-		scene.attachChild(title);
-		title.setPosition(CAMERA_WIDTH/2 - title.getWidth()/2, 10);
-		final Text level = new Text(0, 0, mSubFont, "Level", this.getVertexBufferObjectManager());
-		scene.attachChild(level);
-		level.setPosition(10, 50);
+		final Text title = new Text(0, 0, getFontTitle(), "Main", this.getVertexBufferObjectManager());
+		final Text level_text = new Text(0, 0, mSubFont, "Level", this.getVertexBufferObjectManager());
 		final Text level_number = new Text(0, 0, mSubFont, "", 2, this.getVertexBufferObjectManager());
-		level.attachChild(level_number);
-		level_number.setText("1");
-		level_number.setPosition(level.getWidth()+10, 0);
 		final Text soal = new Text(0, 0, mSubFont, "/10", this.getVertexBufferObjectManager());
-		scene.attachChild(soal);
-		soal.setPosition(CAMERA_WIDTH - (soal.getWidth()+10), 50);
 		final Text soal_number = new Text(0, 0, mSubFont, "", 2, this.getVertexBufferObjectManager());
 		final Text question = new Text(0, 0, mQuestionFont, "", 200, new TextOptions(HorizontalAlign.CENTER), this.getVertexBufferObjectManager());
+		
+		attach(title, Alignment.TOP_CENTER);
+		attach(level_text, Alignment.LEFT_TOP);set_position(level_text, 10, 25);
+		attach(soal, Alignment.RIGHT_TOP);set_position(soal, -10, 25);
+		
+		level_text.attachChild(level_number);
+		level_number.setText(""+level);
+		level_number.setPosition(level_text.getWidth()+10, 0);
+		
 		soal.attachChild(soal_number);
-		question_frame.attachChild(question);
-		question.setPosition(question_frame.getWidth()/2 - question.getWidth()/2, question_frame.getHeight()/2 - question.getHeight()/2);
+		
+		attach(question, Alignment.CENTER, question_frame);
+		
 		final Text[] options = new Text[OPTIONS_COUNT];
 		final Sprite[] options_frame = new Sprite[OPTIONS_COUNT];
 		for(int i = 0;i < OPTIONS_COUNT;i++){
-			final int max_update = questions.length;
+			final int max_update = QUESTIONS.length;
 			options_frame[i] = new Sprite(0, 0, this.mOptionsFrameTextureRegion[i], this.getVertexBufferObjectManager()){
 				@Override
 				public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
@@ -209,26 +92,24 @@ public class MainActivity extends SimpleBaseGameActivity implements Constants{
 			};
 			options[i] = new Text(0, 0, mQuestionFont, "", 40, new TextOptions(HorizontalAlign.CENTER), this.getVertexBufferObjectManager());
 			options_frame[i].attachChild(options[i]);
-			scene.attachChild(options_frame[i]);
-			scene.registerTouchArea(options_frame[i]);
+			attach(options_frame[i], Alignment.BOTTOM_CENTER);
+			getScene().registerTouchArea(options_frame[i]);
 		}
-		options_frame[0].setPosition(CAMERA_WIDTH/4 - options_frame[0].getWidth()/2, 245 - options_frame[0].getHeight()/2);
-		options_frame[1].setPosition(CAMERA_WIDTH/4 - options_frame[1].getWidth()/2, 290 - options_frame[1].getHeight()/2);
-		options_frame[2].setPosition(CAMERA_WIDTH/2 + CAMERA_WIDTH/4 - options_frame[2].getWidth()/2, 245 - options_frame[2].getHeight()/2);
-		options_frame[3].setPosition(CAMERA_WIDTH/2 + CAMERA_WIDTH/4 - options_frame[3].getWidth()/2, 290 - options_frame[3].getHeight()/2);
+		set_position(options_frame[0], -55, -55);
+		set_position(options_frame[1], 55, -55);
+		set_position(options_frame[2], -55, -10);
+		set_position(options_frame[3], 55, -10);
 		
 		update_question(question_number, question, question_frame, soal_number, options, options_frame);
-		
-		return scene;
 	}
 	
 	void update_question(int index, Text question, Sprite parent, Text soal_number, Text[] options, Sprite[] options_frame){
 		soal_number.setText(""+(index+1));
 		soal_number.setPosition(-(soal_number.getWidth()+2), 0);
-		question.setText(questions[index].getQuestion());
+		question.setText(QUESTIONS[index].getQuestion());
 		question.setPosition(parent.getWidth()/2 - question.getWidth()/2, parent.getHeight()/2 - question.getHeight()/2);
 		for(int i = 0;i < OPTIONS_COUNT;i++){
-			options[i].setText(questions[index].getOptions()[i]);
+			options[i].setText(QUESTIONS[index].getOptions()[i]);
 			options[i].setPosition(options_frame[i].getWidth()/2 - options[i].getWidth()/2, options_frame[i].getHeight()/2 - options[i].getHeight()/2);
 		}
 	}
